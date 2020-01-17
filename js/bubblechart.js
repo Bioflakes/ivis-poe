02b29a5d976d881f0348ff769c3987b4c1ab4f71
DamirGrgic <damir.grgic@students.fhnw.ch>
2020-01-17 15:00:23 +0100

    fix slider

--- js/bubblechart.js
+++ js/bubblechart.js
@@ -43,7 +43,7 @@ function bubblechart() {
     readClassFile();
 }
 
-function changeDatasetBubbles(value) {
+function changeDataset(value) {
     dataset_bubbles = "data/minimized/ClassFixed_MOCKDATA_v3_min_" + value + ".csv";
 
     skill_groups = null;
